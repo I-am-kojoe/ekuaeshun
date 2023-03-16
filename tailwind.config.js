6eefc5c49e065d60d89e97da0255ba86fd4785ae
unknown <creativ.drops@gmail.com>
2023-03-16 23:01:07 +0000

    EKUAESHUN

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ["**/*.{html, js}", "**/**/*.{html, js}", "./index.html"],
+  theme: {
+    textFillColor: theme => theme('borderColor'),
+    textStrokeColor: theme => theme('borderColor'),
+    textStrokeWidth: theme => theme('borderWidth'),
+    paintOrder: {
+      'fsm': { paintOrder: 'fill stroke markers' },
+      'fms': { paintOrder: 'fill markers stroke' },
+      'sfm': { paintOrder: 'stroke fill markers' },
+      'smf': { paintOrder: 'stroke markers fill' },
+      'mfs': { paintOrder: 'markers fill stroke' },
+      'msf': { paintOrder: 'markers stroke fill' },
+    },
+    screens: {
+      'xs': '361px',
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors: {
+        'primary-color': '#0fa',
+        'bg-color': '#0A0D0E',
+        'custom-grey': '#dbdbdb',
+        'svg-color': '#666868',
+        'sec-color-1': '#f5b03e',
+        'sec-color-2': '#f53ec2'
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+      },
+      opacity: {
+        '60': '.60',
+      },
+      fontFamily: {
+        'Satoshi': ['Satoshi', 'sans-serif'],
+        'Dahlia': ['Dahlia', 'serif'] // Ensure fonts with spaces have " " surrounding it.
+      }
+    },
+  },
+  plugins: [require('tailwindcss-text-fill-stroke'),],
+}
